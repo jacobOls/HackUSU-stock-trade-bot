e914a6aa0dd70f86fe51fdf382b30038234ff699
jacobOls <46330868+jacobOls@users.noreply.github.com>
2022-04-06 18:41:21 -0600

    Update StockClass.py

--- StockClass.py
+++ StockClass.py
@@ -22,18 +22,18 @@ class Stock:
         start = datetime.datetime(2020, 12, 1)
         stock_prices = pd.DataFrame(pdr.get_data_yahoo(self.symbol,start,day)['Close'])
         
-        stock_prices['5d'] = stock_prices['Close'].rolling(window=5).mean()
+        stock_prices['20d'] = stock_prices['Close'].rolling(window=20).mean()
         
         stock_prices['return'] = np.log(stock_prices['Close'] / stock_prices['Close'].shift(1))
         
-        stock_prices['difference'] = stock_prices['Close'] - stock_prices['5d']
+        stock_prices['difference'] = stock_prices['Close'] - stock_prices['20d']
         
         stock_prices['hold'] = np.where(stock_prices['difference'] < -3, 1, np.nan)
         stock_prices['hold'] = np.where(stock_prices['difference'] * stock_prices['difference'].shift(1) < 0, 0, stock_prices['hold'])
         
         stock_prices['hold'] = stock_prices['hold'].ffill().fillna(0)
         stock_prices['gain_loss'] = stock_prices['hold'].shift(1) * stock_prices['return']
-        stock_prices = stock_prices.dropna(subset=['5d'])
+        stock_prices = stock_prices.dropna(subset=['20d'])
         
         stock_prices['total'] = stock_prices['gain_loss'].cumsum()
     
@@ -41,7 +41,7 @@ class Stock:
         stock_difference = stock_prices['difference'].to_numpy()
         stock_price = stock_prices['Close'].to_numpy()
         stock_trend = stock_prices['difference'].to_numpy()
-        stock_mean = stock_prices['5d'].to_numpy()
+        stock_mean = stock_prices['20d'].to_numpy()
     
         self.return_value = stock_return[-1]
         self.difference = stock_difference[-1]
@@ -51,11 +51,13 @@ class Stock:
         
     def sell(self):
         global money
-        if self.return_value <= 0.01 and abs(self.difference) > 2:
+        if self.price >= self.mean * 1.04 or self.price <= self.mean * 0.7:
             money += self.count * self.price
+            old = self.count
             self.count = 0
-            print("sold", self.symbol, self.count,money)
-            self.last_sold = True
+            print("Sold", old,self.symbol, "Current Balance:",money)
+            if self.price <= self.mean * 0.8:
+                self.last_sold = True
             return True
         return False
         
@@ -67,7 +69,7 @@ class Stock:
         for i in refresh_sold:
             i.last_sold = False
         self.last_sold = True
-        print("bought",self.symbol, self.count,money)
+        print("Bought",self.count, self.symbol)
 
 
 
@@ -77,17 +79,26 @@ def execute(stocks,day):
     change_in_money = 0
     first_loop = True
     money_points = []
+    starting_balance = 0
+    for i in stocks:
+        i.get_values(day[0])
+        starting_balance += i.count * i.price
+    portfolio = starting_balance
     for k in day:
         for i in stocks:
             i.get_values(k)
 
         for i in stocks:
+            if i.count > 0:
+                portfolio = i.price * i.count + money
+
+        for i in stocks:
             if i.count > 0 and not first_loop:
-                change_in_money += (i.price - i.last_price)
-                print(i.price,i.difference,change_in_money)
+                change_in_money = (portfolio - starting_balance)
+                print("Stock Price:",int(i.price),"Stock Mean:", int(i.mean),"Money Earned:",int(change_in_money),"Portfolio:",int(portfolio))
             elif i.count > 0 and first_loop:
                 first_loop = False
-                print(i.price,i.difference,change_in_money)
+                print("Stock Price:",int(i.price),"Stock Mean:", int(i.mean),"Money Earned:",int(change_in_money),"Portfolio:",int(portfolio))
 
         for i in stocks:
             if i.count > 0:
@@ -98,7 +109,7 @@ def execute(stocks,day):
             lowest_trend = math.inf
             for i in stocks:
                 if i.last_sold == False:
-                    if i.difference < lowest_trend and i.return_value >= 0 and i.difference > 0.25:
+                    if i.difference < lowest_trend and i.price < i.mean and money > i.price:
                         for j in stocks:
                             j.lowest_stock = False
                         lowest_trend = i.difference
@@ -110,8 +121,7 @@ def execute(stocks,day):
         for i in stocks:
             if i.count > 0:
                 money_points.append(i.price * i.count + money)
-        for i in stocks:
-            i.last_price = i.price
+
     return i.count, i.symbol, i.price, change_in_money, money, money_points
 
             
